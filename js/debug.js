e2447822848a6e4eafb705197b22d286bab127aa
haawwkeye <connorbishop53@gmail.com>
2022-11-19 18:13:26 -0500

    debug args

--- js/debug.js
+++ js/debug.js
@@ -2,7 +2,6 @@ let debug = {
     enabled: true,
     log: (...args) => {
         if (!debug.enabled) return;
-        let str = args.toString();
-        console.log(`[DEBUG] ${str}`);
+        console.log(`[DEBUG] `, args);
     }
 }
