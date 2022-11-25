2842f2e5534b25a7d03855f48b7ec26a54f9f89a
haawwkeye <connorbishop53@gmail.com>
2022-11-25 16:11:12 -0500

    add debug warn

--- js/debug.js
+++ js/debug.js
@@ -4,6 +4,10 @@ let debug = {
         if (!debug.enabled) return;
         console.log(...data);
     },
+    warn: (...data) => {
+        if (!debug.enabled) return;
+        console.warn(...data);
+    },
     error: (...data) => {
         if (!debug.enabled) return;
         console.error(...data);
