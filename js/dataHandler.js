ef991d4e4852816bb0b08d6b3b7002d14842cb22
haawwkeye <connorbishop53@gmail.com>
2022-11-30 00:29:03 -0500

    loop func, upgrades?, make code look abit better

--- js/dataHandler.js
+++ js/dataHandler.js
@@ -4,6 +4,11 @@ let defaultData = {
     clicksleft: 10, // Clicks left
     moneygain: 0, // Money gain
 
+    upgrades: { // Upgrades for the game
+        unknown1: 0, // ???
+        unknown2: 0, // ???
+    },
+
     settings: { // Settings for the game
         MuteBGAudio: false, // If BG Music is muted
         debugEnabled: false, // If debug mode is enableds
@@ -13,6 +18,7 @@ let defaultData = {
 
     date: new Date().getTime()/1000 // Date in seconds
 } // default data
+
 let currentData;
 
 async function getFile() {
@@ -62,24 +68,36 @@ async function getFile() {
 
 async function saveAs(content) {
     if (window.showSaveFilePicker) {
-      const handle = await showSaveFilePicker({
-        suggestedName: "SaveData.json",
-        types: [{
-            description: "Save Data",
-            accept: {"application/json": [".json"]}
-        }]
-      });
-      const writable = await handle.createWritable();
-      await writable.write(content);
-      writable.close();
+        const handle = await showSaveFilePicker({
+            suggestedName: "SaveData.json",
+            types: [{
+                description: "Save Data",
+                accept: {"application/json": [".json"]}
+            }]
+        });
+
+        const writable = await handle.createWritable();
+        await writable.write(content);
+        writable.close();
     } else {
-      const save = document.createElement("a");
-      save.href = URL.createObjectURL(content);
-      save.download = "SaveData.json";
-      save.click();
-      setTimeout(() => URL.revokeObjectURL(save.href), 60000);
+        const save = document.createElement("a");
+        
+        save.href = URL.createObjectURL(content);
+        save.download = "SaveData.json";
+        save.click();
+
+        setTimeout(() => URL.revokeObjectURL(save.href), 60000);
     }
-  }
+}
+
+function loop(object, callback) {
+    for (const key in object) {
+        if (Object.hasOwnProperty.call(object, key)) {
+            const element = object[key];
+            callback(key, element);
+        }
+    }
+}
 
 let dataHandler = {
     /**
@@ -88,12 +106,15 @@ let dataHandler = {
      */
     checkForData: () => {
         let data = localStorage.getItem("data");
+
         try {
             if (data) data = JSON.parse(data);
         } catch (error) {
             data = null;
         }
+
         if (!data) data = defaultData;
+        
         return data;
     },
     /**
@@ -102,9 +123,11 @@ let dataHandler = {
      */
     getData: (key) => {
         if (!currentData) currentData = dataHandler.validate(dataHandler.checkForData());
+        
         if (key) {
             return currentData[key];
         }
+        
         return currentData;
     },
     /**
@@ -114,8 +137,10 @@ let dataHandler = {
     saveData: (data, showMsg) => {
         if (data) currentData = dataHandler.validate(data);
         else currentData = dataHandler.getData(); // validate happens on get
+        
         currentData.date = new Date().getTime()/1000
         localStorage.setItem("data", JSON.stringify(currentData));
+        
         if (showMsg) {
             try {
                 document.querySelector("#SavingText").style.opacity = 1; 
@@ -124,6 +149,7 @@ let dataHandler = {
                 }, 2500);
             } catch (error) {}
         }
+        
         return currentData;
     },
     /**
@@ -131,19 +157,26 @@ let dataHandler = {
      * @param {object?} data 
      */
     validate: (data) => {
-        for (const elem in defaultData) {
-            if (Object.hasOwnProperty.call(defaultData, elem)) {
-                if (data[elem]) continue;
-                data[elem] = defaultData[elem];
-            }
-        }
-        for (const elem in defaultData.settings) {
-            if (Object.hasOwnProperty.call(defaultData.settings, elem)) {
-                if (data.settings[elem]) continue;
-                data.settings[elem] = defaultData.settings[elem];
-            }
-        }
+        loop(defaultData, (i, v) => {
+            if (data[i]) return;
+            data[i] = v;
+        });
+        
+        loop(defaultData.settings, (i, v) => {
+            if (data.settings[i]) return;
+            data.settings[i] = v;
+        });
+
+        loop(defaultData.upgrades, (i, v) => {
+            if (data.upgrades[i]) return;
+            data.upgrades[i] = v;
+        });
+
         return data;
+    },
+    wipeData: () => {
+        // TODO: Test to make sure this works 
+        dataHandler.Load(defaultData, true);
     },
     // Import/Export stuff
     Import: async () => {
